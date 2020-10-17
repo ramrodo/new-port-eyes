0e5ba0868ed34cf5359d0089e199437250f17e90
Rodolfo Martínez Vega <rodomar@outlook.com>
2020-10-17 03:23:49 -0500

    clean code

--- ui-new-port-eyes/src/components/Busqueda/Busqueda.js
+++ ui-new-port-eyes/src/components/Busqueda/Busqueda.js
@@ -2,44 +2,67 @@ import React, {Fragment, useState} from 'react';
 import './style.css';
 
 const Busqueda = () => {
-    const [datos, setDatos] = useState({
-        variable: '',
-        nivel: '',
-        medida: '',
+  const [datos, setDatos] = useState({
+    variable: '',
+    nivel: '',
+    medida: '',
+  })
+
+  const handleInputChange = (event) => {
+    setDatos({
+      ...datos,
+      [event.target.name] : event.target.value
     })
+  }
 
-    const handleInputChange = (event) => {
-        setDatos({
-            ...datos,
-            [event.target.name] : event.target.value
-        })
-    }
+  const enviarDatos = (event) => {
+    event.preventDefault()
+    console.log('enviando datos...' + datos.variable + ' ' + datos.nivel + ' ' + datos.medida)
+  }
 
-    const enviarDatos = (event) => {
-        event.preventDefault()
-        console.log('enviando datos...' + datos.variable + ' ' + datos.nivel + ' ' + datos.medida)
-    }
+  return (
+    <Fragment>
+        <form className="row" onSubmit={enviarDatos}>
+          <div className="col align-self-center" id="listas">
+            <div className="lista1">
+              <h2>Variable</h2>
+              <select name="OS">
+                <option defaultValue="0"> Elige una opción </option>
+                <option value="1">Windows Vista</option>
+                <option value="2">Windows 7</option>
+                <option value="3">Windows XP</option>
+              </select>
+            </div>
 
-    return (
-        <Fragment>
-            <h1>Formulario</h1>
-            <form className="row" onSubmit={enviarDatos}>
-                <div className="col-md-3">
-                    <input type="text" placeholder="Variable" className="form-control" onChange={handleInputChange} name="variable"></input>
-                </div>
-                <div className="col-md-3">
-                    <input type="text" placeholder="Nivel" className="form-control" onChange={handleInputChange} name="nivel"></input>
-                </div>
-                <div className="col-md-3">
-                    <input type="text" placeholder="Medida" className="form-control" onChange={handleInputChange} name="nivel"></input>
-                </div>
-                <button type="submit" className="btn btn-primary">Enviar</button>
-            </form>
-            <ul>
-                <li>{datos.variable}</li>
-                <li>{datos.nivel}</li>
-                <li>{datos.medida}</li>
-            </ul>
+            <div className="lista2">
+              <h2>  Nivel  </h2>
+              <select name="OS">
+                <option defaultValue="0"> Elige una opción </option>
+                <option value="1">Windows Vista</option>
+                <option value="2">Windows 7</option>
+                <option value="3">Windows XP</option>
+              </select>
+            </div>
+
+            <div className="lista3">
+              <h2>  Medida  </h2>
+              <select name="OS">
+                <option defaultValue="0"> Elige una opción </option>
+                <option value="1">Windows Vista</option>
+                <option value="2">Windows 7</option>
+                <option value="3">Windows XP</option>
+              </select>
+            </div>
+
+            <button type="submit" className="submit"><center>Buscar</center></button>
+          </div>
+        </form>
+
+        <ul>
+            <li>{datos.variable}</li>
+            <li>{datos.nivel}</li>
+            <li>{datos.medida}</li>
+        </ul>
         </Fragment>
     );
 }
